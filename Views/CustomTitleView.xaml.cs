a5543667be5b804f72976d20a557731b9a2a27d6
Richard <rcoric03@gmail.com>
2024-02-17 19:58:58 +0000

    Add more pages and isolate the title bar

--- Views/CustomTitleView.xaml.cs
+++ Views/CustomTitleView.xaml.cs
@@ -1,14 +1,14 @@
-namespace iMate;
+namespace iMate.Views;
 
-public partial class JournalPage : ContentPage
+public partial class CustomTitleView : ContentView
 {
-	public JournalPage()
+	public CustomTitleView()
 	{
 		InitializeComponent();
 	}
-
     private async void ImageButton_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new ProfilePage());
     }
+
 }
