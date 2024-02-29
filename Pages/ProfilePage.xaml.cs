0937e8c593f48c2284a2314da07dbbd690b89eab
Richard <rcoric03@gmail.com>
2024-02-29 19:29:43 +0000

    Add Profile Page - Day

--- Pages/ProfilePage.xaml.cs
+++ Pages/ProfilePage.xaml.cs
@@ -2,10 +2,10 @@ namespace iMate.Pages;
 
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage()
-	{
-		InitializeComponent();
-	}
+    public ProfilePage()
+    {
+        InitializeComponent();
+    }
 
 
     // TEMPORARY WAY TO NAVIGATE TO SETTINGS!!!!!
@@ -13,5 +13,16 @@ public partial class ProfilePage : ContentPage
     {
         Navigation.PushAsync(new SettingsPage());
     }
+
+    private void UsernameTapped(object sender, EventArgs e)
+    {
+
+    }
+
+    private void PersonalInformationTapped(object sender, EventArgs e) { }
+
+    private void NotificationsTapped(object sender, EventArgs e) { }
+
+    private void FeedbackTapped(object sender, EventArgs e) { }
 
 }
