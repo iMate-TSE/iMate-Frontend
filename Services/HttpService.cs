97712492ca7cd129b99fcff400aac3edf52bf7c1
Richard <rcoric03@gmail.com>
2024-04-12 20:11:14 +0100

    getprofile backend

--- Services/HttpService.cs
+++ Services/HttpService.cs
@@ -6,18 +6,45 @@ namespace iMate.Services
     // Singleton
     public interface IHttpService
     {
-        
+        Task<User> FetchProfile(string token);
     }
 
     class HttpService : IHttpService
     {
         private static readonly HttpClient _httpClient = new()
         {
-            BaseAddress = new Uri("http://10.0.2.2:5137/api/")
+            BaseAddress = new Uri("http://10.0.2.2:5137/api/v1/")
         };
 
-        public HttpService() { }
-        
+        public HttpService()
+        {
+        }
+
+        public async Task<User> FetchProfile(string token)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync($"Profile/?token={token}");
+
+                response.EnsureSuccessStatusCode();
+
+                var jsonResponse = await (response.Content.ReadFromJsonAsync<User>());
+                
+                if (jsonResponse != null)
+                {
+                    return jsonResponse;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
     }
-    
 }
