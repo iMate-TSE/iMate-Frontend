e840d968a80915cb06671be21721604dcca14522
Richard <rcoric03@gmail.com>
2024-04-12 13:25:18 +0100

    Add slider viewmodel

--- Pages/SliderPage.xaml.cs
+++ Pages/SliderPage.xaml.cs
@@ -1,9 +1,13 @@
-namespace iMate.Pages;
-
-public partial class SliderPage : ContentPage
+namespace iMate.Pages
 {
-	public SliderPage()
-	{
-		InitializeComponent();
-	}
+    public partial class SliderPage : ContentPage
+    {
+        public SliderPage()
+        {
+            InitializeComponent();
+            BindingContext = new SliderPageViewModel(); // connect the mvvm to sliders page
+        }
+        
+    }
 }
+    
---
+++ ViewModels/SliderPageViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace iMate.ViewModels;
+
+// ViewModel class
+public partial class SliderPageViewModel : ObservableObject
+{ 
+    
+    // Slider values
+    [ObservableProperty]
+    private double _firstslidervalue = 10; 
+
+    private double _secondslidervalue = 50;
+    
+            // Commands
+    public ICommand RedButtonCommand { get; private set; }
+    public ICommand BlueButtonCommand { get; private set; }
+    public ICommand YellowButtonCommand { get; private set; }
+
+    public SliderPageViewModel()
+    {
+        InitializeCommands();
+    }
+
+    private void InitializeCommands()
+    {
+        RedButtonCommand = new Command(ExecuteRedButtonCommand);
+        BlueButtonCommand = new Command(ExecuteBlueButtonCommand);
+        YellowButtonCommand = new Command(ExecuteYellowButtonCommand);
+    }
+
+    private void ExecuteRedButtonCommand(object obj)
+    {
+                
+    }
+
+    private void ExecuteBlueButtonCommand(object obj)
+    {
+                
+    }
+
+    private void ExecuteYellowButtonCommand(object obj) 
+    {
+               
+    }
+            
+            
+    /*
+        in the execute button commands bit i didn't know exactly what to put as this is wehere i assume we feed data into the database and 2i wasn't woking on that so i don't know how you want to do it
+        i assume something like:
+    
+        private void Execute(colour)ButtonCommand(object obj)
+       {
+            // Save data to the database
+            var databaseService = new DatabaseService();
+            var ( database ) = new databaseentry
+            database.insertmoodedate (firstslidervalue, secondslidervalue, "(colour)");   ?????
+        }
+    */
+        }
